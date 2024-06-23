558994d1617bbf42c1c01c3e20132d2dad46c262
Ronalk <sam.ronaldurbano@gmail.com>
2024-06-23 11:18:14 -0500

    Estructura de T2

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL2SOAP_URBANO_CHAVEZ_RONALD/DswiiCl2SoapUrbanoChavezRonaldApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL2SOAP_URBANO_CHAVEZ_RONALD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl2SoapUrbanoChavezRonaldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl2SoapUrbanoChavezRonaldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL2SOAP_URBANO_CHAVEZ_RONALD/DswiiCl2SoapUrbanoChavezRonaldApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL2SOAP_URBANO_CHAVEZ_RONALD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl2SoapUrbanoChavezRonaldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
